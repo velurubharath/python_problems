f06b656226a4729577cac565bfd3954fd1c40d3c
velurubharath <37496166+velurubharath@users.noreply.github.com>
2026-06-14 00:56:40 +0530

    Commited modules

---
+++ Warmup/modules.py
@@ -0,0 +1,20 @@
+import os
+import subprocess
+
+print(os.getcwd())
+print(os.listdir())
+#os.mkdir("test_dir")
+
+#os.rmdir("test_dir")
+
+
+#Subprocess module
+
+# output = subprocess.run(
+#     ["dir"],
+#     capture_output=True,
+#     text=True
+# )
+# print(output.stdout)
+
+# subprocess.run(["kubectl","get","pods"])
